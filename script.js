716f0e5d8f9ebfe7d587116fcd0c38dde508fd77
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-15 16:49:25 +0800

    added item

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const myDataElement = document.getElementById('myData');
+    const myDataElement = document.getElementById('data');
 
     data.forEach(item => {
       const itemElement = document.createElement('div');
+      itemElement.classList.add('data__wrapper-result__item');
       itemElement.innerHTML = `
-        <p>Name: ${item.category}</p>
-        <p>Age: ${item.score}</p>
-        <p>City: ${item.icon}</p>
+        <img src="${item.icon}" alt="${item.name} icon"/>
+        <p>${item.category}</p>
+        <p>${item.score} / 100</p>
       `;
       myDataElement.appendChild(itemElement);
     });
