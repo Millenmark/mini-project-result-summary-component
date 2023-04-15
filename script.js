2d787fd7c2ed561cc060aa61633a3bba985f1860
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-15 20:49:57 +0800

    added average

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+let average = document.getElementById('average')
+
+
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
+    let totalScore = 0;
     const myDataElement = document.getElementById('data');
-
+    let length = data.length;
     data.forEach(item => {
       const itemElement = document.createElement('div');
       itemElement.classList.add('data__wrapper-result__item');
@@ -14,5 +18,11 @@ fetch('data.json')
         <p><span>${item.score}</span> / 100</p>
       `;
       myDataElement.appendChild(itemElement);
+      totalScore += item.score;
+      average.innerText = getAverage(totalScore, length);
     });
   });
+
+  function getAverage(total, dividend) {
+    return Math.floor(total / dividend)
+  }
