53ec953051a88bc27f558826732a3e72ff287093
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-15 17:01:51 +0800

    added styles for data items

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ fetch('data.json')
       const itemElement = document.createElement('div');
       itemElement.classList.add('data__wrapper-result__item');
       itemElement.innerHTML = `
-        <img src="${item.icon}" alt="${item.name} icon"/>
-        <p>${item.category}</p>
+        <div>
+          <img src="${item.icon}" alt="${item.name} icon"/>
+          <p>${item.category}</p>
+        </div>
         <p>${item.score} / 100</p>
       `;
       myDataElement.appendChild(itemElement);
